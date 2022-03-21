e4226f34792df42cc90695745e6d7a733074dbbb
Richard Nkulu <64165035+RichardNk24@users.noreply.github.com>
2022-03-21 11:50:52 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var settingsMenu = document.querySelector(".settings-menu");
-var darkBtn = document.getElementById("dark-btn"                                                                           );
+var darkBtn = document.getElementById("dark-btn"                                                                          );
 
 function settingsMenuToggle(){
     settingsMenu.classList.toggle("settings-menu-height");   
